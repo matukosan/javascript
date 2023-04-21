6b928a7f3764ec1945281be2031fb2545c083073
panteliselef <panteliselef@outlook.com>
2023-04-21 19:18:54 +0300

    fix(clerk-js): Refactor FormControl to correctly handle edge cases
    This refactoring handles the following cases:
    - Phone input in firefox has incorrect height
    - For checkboxes we first display input then the label
    - The animated label below an input (error or success) will not move the field itself

--- packages/clerk-js/src/ui/elements/FormControl.tsx
+++ packages/clerk-js/src/ui/elements/FormControl.tsx
@@ -12,7 +12,7 @@ import {
   FormLabel,
   FormSuccessText,
   FormText,
-  Icon,
+  Icon as IconCustomizable,
   Input,
   Link,
   localizationKeys,
@@ -171,6 +171,83 @@ export const FormControl = forwardRef<HTMLInputElement, FormControlProps>((props
     return true;
   }, [enableErrorAfterBlur, hasLostFocus, submittedWithEnter]);
 
+  const ActionLabel = actionLabel ? (
+    <Link
+      localizationKey={actionLabel}
+      elementDescriptor={descriptors.formFieldAction}
+      elementId={descriptors.formFieldLabel.setId(id)}
+      isDisabled={isDisabled}
+      colorScheme='primary'
+      onClick={e => {
+        e.preventDefault();
+        onActionClicked?.(e);
+      }}
+    >
+      {typeof actionLabel === 'string' ? actionLabel : undefined}
+    </Link>
+  ) : null;
+
+  const HintLabel =
+    isOptional && !actionLabel ? (
+      <Text
+        localizationKey={localizationKeys('formFieldHintText__optional')}
+        elementDescriptor={descriptors.formFieldHintText}
+        elementId={descriptors.formFieldHintText.setId(id)}
+        as='span'
+        colorScheme='neutral'
+        variant='smallRegular'
+        isDisabled={isDisabled}
+      />
+    ) : null;
+
+  // TODO: This is a temporary fix. Replace this when the tooltip component is introduced
+  const Icon = icon ? (
+    <Flex
+      as={'span'}
+      title='A slug is a human-readable ID that must be unique.  It’s often used in URLs.'
+    >
+      <IconCustomizable
+        icon={icon}
+        sx={theme => ({
+          marginLeft: theme.space.$0x5,
+          color: theme.colors.$blackAlpha400,
+          width: theme.sizes.$4,
+          height: theme.sizes.$4,
+        })}
+      />
+    </Flex>
+  ) : null;
+
+  const FieldLabel = (
+    <FormLabel
+      localizationKey={typeof label === 'object' ? label : undefined}
+      elementDescriptor={descriptors.formFieldLabel}
+      elementId={descriptors.formFieldLabel.setId(id)}
+      hasError={hasError}
+      isDisabled={isDisabled}
+      isRequired={isRequired}
+      sx={{
+        display: 'flex',
+        alignItems: 'center',
+      }}
+    >
+      {typeof label === 'string' ? label : undefined}
+    </FormLabel>
+  );
+
+  const Input = (
+    <InputElement
+      elementDescriptor={descriptors.formFieldInput}
+      elementId={descriptors.formFieldInput.setId(id)}
+      hasError={hasError}
+      isDisabled={isDisabled}
+      isRequired={isRequired}
+      {...rest}
+      ref={ref}
+      placeholder={t(placeholder)}
+    />
+  );
+
   return (
     <FormControlPrim
       elementDescriptor={descriptors.formField}
@@ -184,94 +261,45 @@ export const FormControl = forwardRef<HTMLInputElement, FormControlProps>((props
       setSuccessful={setSuccessful}
       sx={sx}
     >
-      <Flex
-        direction={isCheckbox ? 'row' : 'columnReverse'}
-        sx={
-          {
-            // Setting height to 100% fixes issue with Firefox for our PhoneInput
-            // height: '100%',
-          }
-        }
-      >
-        <InputElement
-          elementDescriptor={descriptors.formFieldInput}
-          elementId={descriptors.formFieldInput.setId(id)}
-          hasError={hasError}
-          isDisabled={isDisabled}
-          isRequired={isRequired}
-          {...rest}
-          ref={ref}
-          placeholder={t(placeholder)}
-        />
-        <Flex
-          justify={icon ? 'start' : 'between'}
-          align='center'
-          elementDescriptor={descriptors.formFieldLabelRow}
-          elementId={descriptors.formFieldLabelRow.setId(id)}
-          sx={theme => ({
-            marginBottom: isCheckbox ? 0 : theme.space.$1,
-            marginLeft: !isCheckbox ? 0 : theme.space.$1,
-          })}
-        >
-          <FormLabel
-            localizationKey={typeof label === 'object' ? label : undefined}
-            elementDescriptor={descriptors.formFieldLabel}
-            elementId={descriptors.formFieldLabel.setId(id)}
-            hasError={hasError}
-            isDisabled={isDisabled}
-            isRequired={isRequired}
-            sx={{
-              display: 'flex',
-              alignItems: 'center',
-            }}
+      {isCheckbox ? (
+        <Flex direction={'row'}>
+          {Input}
+          <Flex
+            justify={icon ? 'start' : 'between'}
+            align='center'
+            elementDescriptor={descriptors.formFieldLabelRow}
+            elementId={descriptors.formFieldLabelRow.setId(id)}
+            sx={theme => ({
+              marginBottom: isCheckbox ? 0 : theme.space.$1,
+              marginLeft: !isCheckbox ? 0 : theme.space.$1,
+            })}
           >
-            {typeof label === 'string' ? label : undefined}
-          </FormLabel>
-          {icon && (
-            // TODO: This is a temporary fix. Replace this when the tooltip component is introduced
-            <Flex
-              as={'span'}
-              title='A slug is a human-readable ID that must be unique.  It’s often used in URLs.'
-            >
-              <Icon
-                icon={icon}
-                sx={theme => ({
-                  marginLeft: theme.space.$0x5,
-                  color: theme.colors.$blackAlpha400,
-                  width: theme.sizes.$4,
-                  height: theme.sizes.$4,
-                })}
-              />
-            </Flex>
-          )}
-          {isOptional && !actionLabel && (
-            <Text
-              localizationKey={localizationKeys('formFieldHintText__optional')}
-              elementDescriptor={descriptors.formFieldHintText}
-              elementId={descriptors.formFieldHintText.setId(id)}
-              as='span'
-              colorScheme='neutral'
-              variant='smallRegular'
-              isDisabled={isDisabled}
-            />
-          )}
-          {actionLabel && (
-            <Link
-              localizationKey={actionLabel}
-              elementDescriptor={descriptors.formFieldAction}
-              elementId={descriptors.formFieldLabel.setId(id)}
-              isDisabled={isDisabled}
-              colorScheme='primary'
-              onClick={e => {
-                e.preventDefault();
-                onActionClicked?.(e);
-              }}
-            >
-              {typeof actionLabel === 'string' ? actionLabel : undefined}
-            </Link>
-          )}
+            {FieldLabel}
+            {Icon}
+            {HintLabel}
+            {ActionLabel}
+          </Flex>
         </Flex>
-      </Flex>
+      ) : (
+        <>
+          <Flex
+            justify={icon ? 'start' : 'between'}
+            align='center'
+            elementDescriptor={descriptors.formFieldLabelRow}
+            elementId={descriptors.formFieldLabelRow.setId(id)}
+            sx={theme => ({
+              marginBottom: isCheckbox ? 0 : theme.space.$1,
+              marginLeft: !isCheckbox ? 0 : theme.space.$1,
+            })}
+          >
+            {FieldLabel}
+            {Icon}
+            {HintLabel}
+            {ActionLabel}
+          </Flex>
+          {Input}
+        </>
+      )}
 
       {isSomeMessageVisible && (
         <Box
